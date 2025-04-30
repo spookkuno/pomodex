0619497a67935efc480fce31c7b71d11bc7c8ff5
spook <130260992+spookkuno@users.noreply.github.com>
2025-04-29 21:51:12 -0400

    Add files via upload
    updated timer screen + shop

--- pomodex/script.js
+++ pomodex/script.js
@@ -1,4 +1,3 @@
-
 let coins = 50;
 
 function updateCoins() {
@@ -93,3 +92,98 @@ function launchConfetti() {
         origin: {y: 0.6}
     });
 }
+
+
+let pomodoro = document.getElementById("pomodoro-timer");
+let short = document.getElementById("short-timer");
+let currentTimer = null;
+
+
+function showDefaultTimer() {
+   pomodoro.style.display = "block";
+   short.style.display = "none";
+}
+
+
+showDefaultTimer()
+
+
+function hideAll() {
+   let timers = document.querySelectorAll(".timer-display");
+   timers.forEach((timer) => (timer.style.display = "none"));
+ }
+
+
+ document
+   .getElementById("pomodoro-session")
+   .addEventListener("click", function () {
+     hideAll();
+
+
+     pomodoro.style.display = "block";
+     currentTimer = pomodoro;
+   });
+ document
+   .getElementById("short-break")
+   .addEventListener("click", function () {
+     hideAll();
+
+
+     short.style.display = "block";
+     currentTimer = short;
+   });
+
+
+
+
+let myInterval = null;
+
+
+function startTimer(timerdisplay) {
+   if (myInterval) {
+       clearInterval(myInterval);
+   }
+   let timerDuration = parseFloat(timerdisplay.getAttribute("data-duration"));
+   console.log(timerDuration);
+   let durationinMiliseconds = timerDuration*60*1000;
+   let endTimestamp = Date.now() + durationinMiliseconds;
+
+
+   myInterval = setInterval(function() {
+       const timeRemaining = new Date(endTimestamp - Date.now());
+
+
+       if (timeRemaining <= 0) {
+          
+           clearInterval(myInterval);
+           timerdisplay.textContent = "00:00";
+           const alarm = new Audio("https://www.freespecialeffects.co.uk/soundfx/scifi/electronic.wav");
+           alarm.play();
+       }
+       else {
+           const minutes = Math.floor(timeRemaining / 60000);
+           const seconds = ((timeRemaining % 60000) / 1000).toFixed(0);
+           const formattedTime = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+           console.log(formattedTime);
+           timerdisplay.textContent = formattedTime;
+       }
+   }, 1000);
+}
+
+
+document.getElementById("start").addEventListener("click", function () {
+   if(currentTimer){
+       startTimer(currentTimer);
+   }
+   else {
+
+
+   }
+});
+
+
+document.getElementById("stop").addEventListener("click", function () {
+   if (currentTimer) {
+     clearInterval(myInterval);
+   }
+});
